a5f9fb2bc91db3ddbcf42959190e14d726b0e865
Ellis Symons <ellis.symons88@gmail.com>
2024-05-15 07:57:15 +0100

    remove redundant code now that fm_to_estry is used

--- pytuflow/results/fm/units/river.py
+++
@@ -1,7 +0,0 @@
-from ....tmf.tmf.tuflow_model_files.db.drivers.river_unit_handler import RiverUnit as River
-from ....tmf.tmf.tuflow_model_files.db.drivers.river_unit_handler import RiverSection
-from ....tmf.tmf.tuflow_model_files.db.drivers.river_unit_handler import RiverCES
-
-
-SUB_UNIT_NAME = 'RIVER'
-AVAILABLE_CLASSES = [River]
