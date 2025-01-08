84c4efd1ff930e00fb261e6c12bc1d56fc825e82
Ellis Symons <ellis.symons@apac.bmt.org>
2025-01-08 12:14:57 +0000

    0.0.95

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
         'pytuflow.tmf.tmf.data': ['*.json'],
         'pytuflow.tmf.tmf.convert_tuflow_model_gis_format.conv_tf_gis_format.data': ['*.json'],
         'fm_to_estry.data': ['*.json'],
+        'pytuflow.outputs.data': ['*.json'],
     },
     include_package_data=True,
 )
