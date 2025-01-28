eb5dfbb8ebec97c9f4e410f342afa50f4591d04c
Ellis Symons <ellis.symons@apac.bmt.org>
2025-01-28 09:19:43 +0000

    0.0.96

--- pytuflow/outputs/__init__.py
+++ pytuflow/outputs/__init__.py
@@ -12,3 +12,5 @@ from .itime_series_2d import ITimeSeries2D
 from .time_series import TimeSeries
 from .tabular_output import TabularOutput
 from .output import Output
+from .helpers.lp_1d import LP_1D, Connectivity
+from .helpers.lp_1d_fm import LP_1D_FM, Connectivity_FM
