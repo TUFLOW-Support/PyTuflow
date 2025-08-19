7e44ad6c0d2754c117fa0320076b1ef8a2baf864
Ellis Symons <ellis.symons@apac.bmt.org>
2025-08-19 14:39:24 +0100

    revert previous commit

--- pytuflow/_outputs/output.py
+++ pytuflow/_outputs/output.py
@@ -28,7 +28,7 @@ class Output(ABC):
 
     @abstractmethod
     def __init__(self, *fpath: PathLike, **kwargs) -> None:
-        super().__init__(*fpath, **kwargs)
+        super().__init__()
         self._fpath = fpath
         self._loaded = False
 
