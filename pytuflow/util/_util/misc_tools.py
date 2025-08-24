f3600f9145366f7dbeeae9c152c46463875fab70
Ellis Symons <ellis.symons@apac.bmt.org>
2025-08-24 11:24:13 +0100

    Creates a list_depth util and exposes it

--- pytuflow/util/_util/misc_tools.py
+++ pytuflow/util/_util/misc_tools.py
@@ -1,4 +1,5 @@
 import itertools
+import typing
 
 
 def flatten(arr: list[list]) -> list:
@@ -15,3 +16,19 @@ def flatten(arr: list[list]) -> list:
         Flattened 1D list.
     """
     return list(itertools.chain.from_iterable(arr))
+
+
+def list_depth(lst: typing.Iterable) -> int:
+    def is_bottom(lst1: typing.Iterable) -> bool:
+        return all(isinstance(x, (float, int, str)) for x in lst1)
+
+    dep = 1
+    lst_ = lst
+    while not is_bottom(lst_):
+        dep += 1
+        try:
+            lst_ = lst_[0]
+        except (TypeError, IndexError):
+            break
+
+    return dep
