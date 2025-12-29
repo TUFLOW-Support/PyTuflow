69db64fc491b4fa72a0b73b3c76b5f10f11bf565
ellis <ellis.symons88@gmail.com>
2025-12-29 15:46:00 +0000

    start adding regression tests before adding new mesh reading methods.

--- tests/test_map_output.py
+++ tests/test_map_output.py
@@ -606,3 +606,57 @@ class TestNCGrid(unittest.TestCase):
         res = NCGrid(p)
         df = res.section(line, 'h', 0)
         self.assertEqual((18, 2), df.shape)
+
+
+def load_comparison_data(path):
+    with open(path, 'rb') as f:
+        buf = f.read()
+    return np.frombuffer(buf)
+
+
+class TestMeshRegression(unittest.TestCase):
+
+    def test_qgis_vertex_mesh(self):
+        p = './tests/xmdf/EG00_001.xmdf'
+        point = './tests/xmdf/xmdf_point.shp'
+        line = './tests/xmdf/xmdf_line.shp'
+        comp = './tests/regression_test_comparisons/test_qgis_vertex_mesh'
+        with pyqgis():
+            res = XMDF(p)
+
+            # time series
+            a = res.time_series(point, 'water level').reset_index().to_numpy()
+            b = load_comparison_data(f'{comp}_time_series.data').reshape(a.shape)
+            is_close = np.isclose(a, b, equal_nan=True)
+            self.assertTrue(is_close.all())
+
+            # section
+            a = res.section(line, 'water level', 1.).reset_index().to_numpy()
+            b = load_comparison_data(f'{comp}_section.data').reshape(a.shape)
+            is_close = np.isclose(a, b, equal_nan=True)
+            self.assertTrue(is_close.all())
+
+            # profile
+            a = res.profile(point, 'velocity', 1.).reset_index().to_numpy()
+            b = load_comparison_data(f'{comp}_profile.data').reshape(a.shape)
+            is_close = np.isclose(a, b, equal_nan=True)
+            self.assertTrue(is_close.all())
+
+            # curtain scalar
+            a = res.curtain(line, 'z0', 1.).reset_index().to_numpy()
+            b = load_comparison_data(f'{comp}_curtain.data').reshape(a.shape)
+            is_close = np.isclose(a, b, equal_nan=True)
+            self.assertTrue(is_close.all())
+
+            # curtain vector
+            a = res.curtain(line, 'velocity', 1.).reset_index().to_numpy()
+            a = np.column_stack((a[...,:3], np.vstack(a[...,3]), np.vstack(a[...,4]))).astype('f8')
+            b = load_comparison_data(f'{comp}_curtain_vec.data').reshape(a.shape)
+            is_close = np.isclose(a, b, equal_nan=True)
+            self.assertTrue(is_close.all())
+
+    def test_qgis_cell_mesh(self):
+        pass
+
+    def test_qgis_cell_mesh_latlong(self):
+        pass
